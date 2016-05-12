30cd207af97657e6d7530d320c29cad6b17c8767
anthony-shaw <anthonyshaw@apache.org>
2016-05-12 21:06:30 +1000

    Improve matching pattern

--- wntf/algorithm.py
+++ wntf/algorithm.py
@@ -8,6 +8,10 @@ from .graphs import word_cloud as graph
 
 class DiversityAlgorithm(object):
     def __init__(self, log):
+        '''
+        :param log: The `logging` instance
+        :type  log: Instance of the Python log
+        '''
         self.log = log
         with open('exclude_words.txt', 'r') as exclude:
             self.exclude_words = [line.rstrip() for line in exclude]
@@ -29,6 +33,16 @@ class DiversityAlgorithm(object):
     }
 
     def tag(self, lines):
+        '''
+        Tokenize and categorise the words in the collection of
+        text
+
+        :param lines: The list of strings with the text to match
+        :type  lines: ``list`` of ``str``
+
+        :rtype: :class:
+        :return:
+        '''
         try:
             tokenized_words = nltk.word_tokenize(lines)
             return nltk.pos_tag(tokenized_words)
@@ -37,18 +51,47 @@ class DiversityAlgorithm(object):
             raise le
 
     def nouns(self, tagged_text):
+        '''
+        Find all nouns in a collection of tagged text
+        '''
         self.log.debug('Collecting nouns')
         return self.findtags('NN', tagged_text)
 
     def findtags(self, tag_prefix, tagged_text):
+        '''
+        Find all words that match a 'tag' (word type) prefix
+
+        :param tag_prefix: The tag prefix
+        :type  tag_prefix: ``str``
+
+        :param tagged_text: The text to search
+        :type  tagged_text: ``list`` of ``dict``
+        '''
         cfd = nltk.ConditionalFreqDist((tag, word) for (word, tag) in tagged_text
             if tag.startswith(tag_prefix))
-        return dict((tag, cfd[tag].most_common(20)) for tag in cfd.conditions())
+        return dict((tag, cfd[tag].most_common(50)) for tag in cfd.conditions())
 
     def synsets(self, word):
+        '''
+        Get synsets for a given word
+
+        :param word: The word
+        :type  word: ``str``
+
+        :rtype: ``list`` of :class:`Synset`
+        '''
         return wordnet.synsets(word)
 
     def load_wheel(self, wheel, data_file):
+        '''
+        Load the data from a wheel data file
+
+        :param wheel: The target diversity wheel
+        :type  wheel: ``dict``
+
+        :param data_file: The path to the wheel data file
+        :type  data_file: ``str``
+        '''
         with open(data_file, 'r') as wheel_data:
             lines = wheel_data.readlines()
             if len(lines) == 0:
@@ -62,28 +105,60 @@ class DiversityAlgorithm(object):
         print(wheel['pattern'])
 
     def load_wheels(self):
+        '''
+        Load the data for all diversity wheels
+        '''
         for key, wheel in self.wheels.items():
             self.load_wheel(wheel, wheel['data'])
 
     def increment_pattern(self, pattern, word):
+        '''
+        A word has matched a pattern in a wheel, increment the match on the wheel
+        '''
         pattern['count'] = pattern['count'] + 1
         pattern['matches'].append(word)
 
     def match_wheel_word(self, wheel, hypernym):
+        '''
+        Try and match a word against a wheel
+
+        :param wheel: The target wheel
+        :type  wheel: ``dict``
+
+        :param hypernym: The word to match
+        :type  hypernym: ``str``
+
+        :rtype: ``bool``
+        :return: `True` if match, else `False`
+        '''
         for key, patterns in wheel['pattern'].items():
             if key == hypernym:
-                self.increment_pattern(wheel['pattern'], hypernym)
+                self.increment_pattern(patterns, hypernym)
                 return True
             if hypernym in patterns['words']:
-                self.increment_pattern(wheel['pattern'], hypernym)
+                self.increment_pattern(patterns, hypernym)
                 return True
         return False
 
     def match_wheel(self, wheel, synset, word):
+        '''
+        Attempt to match a keyword to a wheel
+
+        :param wheel: The target wheel
+        :type  wheel: ``dict`` wheel
+
+        :param synset: The word synset
+        :type  synset: :class:`Synset`
+
+        :param word: The target word
+        :type  word: ``str``
+
+        :rtype: ``bool``
+        :return: `True` if match, else `False`
+        '''
         # see if each hypernym matches a wheel
         for hypernym in synset.hypernyms():
             proper_name = hypernym.name().split('.')[:1][0]
-            self.log.debug('hypernym %s in %s', proper_name, word)
             if self.match_wheel_word(wheel, proper_name):
                 self.log.debug('matched %s to wheel', proper_name)
                 return True
@@ -119,7 +194,7 @@ class DiversityAlgorithm(object):
                         self.log.warning('%s was not found in the synsets', word)
 
         '''
-        TODO: 
+        TODO:
         - map those in a chord where the size of the difference between the angles in the chord represents the difference,
             e.g. carpenter is very different to IT, but banker is similar to accountant
         '''
